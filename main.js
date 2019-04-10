2f01784d86fab6a36381fa23c6941a0010d73e23
jonathoncoding <36321803+jonathoncoding@users.noreply.github.com>
2019-04-10 14:48:40 -0700

    Goofing around

--- main.js
+++ main.js
@@ -17,15 +17,15 @@
     var tool = new Tool();
 
     tool.onMouseDown = function(event) {
-        var c = Shape.Circle(200, 200, 80);
+        var c = Shape.Circle(200, 200, 100);
         c.fillColor = 'black';
         var text = new PointText(200, 200);
         text.justification = 'center';
         text.fillColor = 'white';
         text.fontSize = 20;
-        text.content = 'hello world';
-        var c = Shape.Circle(event.point, 20);
-        c.fillColor = 'green';
+        text.content = 'Hey, Motor Mouth!';
+        var c = Shape.Circle(event.point, 30);
+        c.fillColor = 'blue';
     };
 
     paper.view.draw();
